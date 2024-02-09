b3f80e1f53966775da84df162aa8ee8e3bb83f15
Nazeer Ahmad <69080659+nazo21@users.noreply.github.com>
2024-02-09 12:35:26 -0500

    Add files via upload

---
+++ Lab1_Partner1 (1).java
@@ -0,0 +1,24 @@
+public class Lab1 {
+    public static void main(String[] args) {
+        System.out.println("    J       A      V     V     A");
+        System.out.println("    J      A A      V   V     A A");
+        System.out.println("J   J     AAAAA      V V     AAAAA");
+        System.out.println(" J J     A     A      V     A     A");
+        System.out.println("++++++++++++++++++++++++++++++");
+        System.out.println("  a    a^2   a^3");
+        System.out.println("  1    1     1");
+        System.out.println("  2    4     8");
+        System.out.println("  3    9     27");
+        System.out.println("  4    16    64");
+        System.out.println("++++++++++++++++++++++++++++++");
+        System.out.println("  a    a^2   a^3");
+        int a = 1;
+        System.out.println("  " + a + "      " + a*a +  "     " + a*a*a);
+        a = 2;
+        System.out.println("  "+a +   "      " + a*a +  "     " + a*a*a);
+        a = 3;
+        System.out.println("  "  + a +"      " + a*a +  "    " + a*a*a);
+        a = 4;
+        System.out.println("  " + a + "     " + a*a +  "    " + a*a*a);
+    }
+}
