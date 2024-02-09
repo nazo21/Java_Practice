a372dfcb5325c341a8dc51938b8529c31f0f7759
Nazeer Ahmad <69080659+nazo21@users.noreply.github.com>
2024-02-09 12:37:50 -0500

    Add files via upload

---
+++ Lab3_Nazeer_Ahmad (2).java
@@ -0,0 +1,45 @@
+//Nazeer Ahmad  2890076
+// Read Three vertices x and y 
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Lab3_Nazeer_Ahmad{
+  public static void main(String[] args){
+         Scanner myInput = new Scanner(System.in);
+         
+         // Input the first vertices
+         System.out.println("Input the first x & y vertices");
+         double x1 = myInput.nextDouble();
+         double y1 = myInput.nextDouble();
+         //double e1 = Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2));
+
+         System.out.println("Input the second x & y vertices");
+         double x2 = myInput.nextDouble();
+         double y2 = myInput.nextDouble();
+         //double e2 = Math.sqrt(Math.pow(x2-x3,2)+Math.pow(y2-y3,2));
+
+         System.out.println("Input the third x & y vertices");
+         double x3 = myInput.nextDouble();
+         double y3 = myInput.nextDouble();
+         
+         
+         double e1 = Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2));
+         double e2 = Math.sqrt(Math.pow(x2-x3,2)+Math.pow(y2-y3,2));
+         double e3 = Math.sqrt(Math.pow(x3-x1,2)+Math.pow(y3-y1,2));
+         
+                 
+         if(e1+e2 > e3 && e2+e3>e1 && e1+e3 > e2){
+            System.out.print("The triangle Perimeter is ");
+            System.out.println(e1+e2+e3);
+         }else{
+         
+               System.out.println("There is an Error!!");
+              }
+        
+    
+  }
+  
+
+}
+
+
